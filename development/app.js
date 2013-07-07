b13d633e9bcd3473a718d4ef6d230f90dd7ccb8b
root <root@admin1.ftrdhcpuser.net>
2013-07-07 13:30:11 -0700

    initial checkin of code

---
+++ development/app.js
@@ -0,0 +1,14 @@
+var message_service = require('message_service');
+
+var config = {
+    host: 'foo.example.com',
+    port: 8124,
+    localAddress: 0
+};
+var service = message_service.createService();
+
+//console.log(serv);
+service.start();
+service.addFriend({host: 'localhost', port: 8124});
+service.sendMessage("I like you");
+service.shutdown();
