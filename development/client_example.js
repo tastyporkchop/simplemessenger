4aebeff0f67f012c134ac7bed42a87dfa166b004
Angus Campbell <angus.b.e.campbell@gmail.com>
2013-07-07 20:36:30 -0700

    some cleanup and added some comments

--- development/client_example.js
+++ development/client_example.js
@@ -1,14 +1,15 @@
 var message_service = require('message_service');
 
-var config = {
-    host: 'foo.example.com',
-    port: 8124,
-    localAddress: 0
-};
+// create the service
 var service = message_service.createService();
 
+// start the service
 service.start();
+
+// add a target (change node1 to ip or dns name of target server)
 service.addFriend({host: 'node1', port: 8124});
+
+// send some data
 service.sendMessage("I like you a whole lot - ");
 service.sendMessage("I like you a whole lot - ");
 service.sendMessage("I like you a whole lot - ");
@@ -40,4 +41,6 @@ service.sendMessage("I like you a whole lot - ");
 service.sendMessage("I like you a whole lot - ");
 service.sendMessage("I like you a whole lot - ");
 service.sendMessage("I like you a whole lot - ");
+
+// shutdown
 service.shutdown();
--- development/server_example.js
+++ development/server_example.js
@@ -1,17 +1,21 @@
 var message_service = require('message_service');
 
-var config = {
-    host: 'foo.example.com',
-    port: 8124,
-    localAddress: 0
-};
+// inline config example for server
+//
+// var config = {
+//     host: 'foo.example.com',
+//     port: 8124,
+//     localAddress: 0
+// };
 
+// create service useing defaults (listen on 0.0.0.0:8124)
 var service = message_service.createService();
 
 // register any message handlers at this level
-// this simple one just prints the received message
+// this simple one just prints the content of received messages
 service.on('received_message', function(message) {
     console.log('application receieved data:'+message);
 });
 
+// start the server (ctrl-c to stop)
 service.start();
