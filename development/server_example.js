501f4e4e4bc723593440e2bb455f77d791d36b83
Angus Campbell <angus.b.e.campbell@gmail.com>
2013-07-07 17:50:37 -0700

    updated server example to use callback

--- development/server_example.js
+++ development/server_example.js
@@ -7,4 +7,11 @@ var config = {
 };
 
 var service = message_service.createService();
+
+// register any message handlers at this level
+// this simple one just prints the received message
+service.on('received_message', function(message) {
+    console.log('application receieved data:'+message);
+});
+
 service.start();
